d2048e92c4f4b6de677a54837fe98cbbd293572d
ninghan <han@swifthealth.cn>
2018-11-10 18:26:30 +0800

    merge

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ import {
 
 const { Signaling } = NativeModules
 
-export default Signaling;
+export default {
+    ...Signaling,
+init(appId = null){
+    Signaling.init(appId);
+},
+login(userId) {
+    this.listener && this.listener.remove();
+    Signaling.login(userId);
+},
+eventEmitter(fnConf) {
+    //there are no `removeListener` for NativeAppEventEmitter & DeviceEventEmitter
+    this.listener && this.listener.remove();
+    this.listener = NativeAppEventEmitter.addListener('SignalingEvent', event => {
+            fnConf[event['type']] && fnConf[event['type']](event);
+});
+},
+removeEmitter() {
+    this.listener && this.listener.remove();
+}
+};
