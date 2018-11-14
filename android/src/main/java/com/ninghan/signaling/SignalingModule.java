c627d71a8d582a5cd658e794e5bf9286d7af9a82
ninghan <han@swifthealth.cn>
2018-11-14 17:03:25 +0800

    signaling callback

--- android/src/main/java/com/ninghan/signaling/SignalingModule.java
+++ android/src/main/java/com/ninghan/signaling/SignalingModule.java
@@ -24,7 +24,6 @@ public class SignalingModule extends ReactContextBaseJavaModule {
     private final String TAG ="logs";
     private boolean stateSingleMode = true; // single mode or channel mode
     private AgoraAPIOnlySignal agoraAPI;
-    private AgoraAPIOnlySignal m_agoraAPI;
     private static String appId = null;
 
     public SignalingModule(ReactApplicationContext context) {
@@ -52,6 +51,7 @@ public class SignalingModule extends ReactContextBaseJavaModule {
     // init callback
     @ReactMethod
     public void init(final String appIds) {
+        if(appIds == null) return;
         appId = appIds;
         agoraAPI =  AgoraAPIOnlySignal.getInstance(getReactApplicationContext(), appId);
         Callback();
@@ -60,6 +60,7 @@ public class SignalingModule extends ReactContextBaseJavaModule {
     // login siginal
     @ReactMethod
     public void login(final String userId) {
+        if(userId == null || userId.equals("")) return;
         agoraAPI.login2(appId, userId, "_no_need_token", 0, "", 5, 1);
         Callback();
     }
@@ -101,6 +102,41 @@ public class SignalingModule extends ReactContextBaseJavaModule {
             agoraAPI.messageChannelSend(channelName, msg, "");
         }
     }
+
+    // invoke rpc method
+    @ReactMethod
+    public void invoke(String name, String req, String callID) {
+        agoraAPI.invoke(name, req, callID);
+    }
+
+    // leave channel by id
+    @ReactMethod
+    public void channelLeave(String channelId) {
+        if(channelId == null || channelId.equals("")) return;
+        agoraAPI.channelLeave(channelId);
+    }
+
+    // query user status
+    @ReactMethod
+    public void queryUserStatus(String account) {
+        if(account == null || account.equals("")) return;
+        agoraAPI.queryUserStatus(account);
+    }
+
+    // query channel user nums
+    @ReactMethod
+    public void channelQueryUserNum(String channelID) {
+        if(channelID == null || channelID.equals("")) return;
+        agoraAPI.channelQueryUserNum(channelID);
+    }
+
+    // invite someone join channnel
+    @ReactMethod
+    public void channelInviteUser2(String channelID, String account, String extras) {
+        if(channelID == null) return;
+        agoraAPI.channelInviteUser2(channelID, account, extras);
+    }
+
 
 
     // login callback
@@ -160,7 +196,6 @@ public class SignalingModule extends ReactContextBaseJavaModule {
                 @Override
                 public void onChannelJoinFailed(final String channelID, final int ecode) {
                     super.onChannelJoinFailed(channelID, ecode);
-
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -172,6 +207,23 @@ public class SignalingModule extends ReactContextBaseJavaModule {
                         }
                     });
                 }
+
+                // leave channel success
+                @Override
+                public void  onChannelLeaved(final String channelID, final int ecode) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onChannelLeaved");
+                            map.putString("channelID", channelID);
+                            map.putInt("ecode", ecode);
+                            commonEvent(map);
+                        }
+                    });
+                }
+
+
 
                 @Override
                 public void onChannelUserList(final String[] accounts, final int[] uids) {
@@ -207,23 +259,23 @@ public class SignalingModule extends ReactContextBaseJavaModule {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            if (i == IAgoraAPI.ECODE_LOGOUT_E_KICKED) { //other login the account
-                                WritableMap map = Arguments.createMap();
-                                map.putString("type", "onLogoutForceout");
-                                map.putInt("code", i);
-                                commonEvent(map);
-
-                            } else if (i == IAgoraAPI.ECODE_LOGOUT_E_NET) { //net
-                                WritableMap map = Arguments.createMap();
-                                map.putString("type", "onLogoutForNetwork");
-                                map.putInt("code", i);
-                                commonEvent(map);
-                            }else {
+//                            if (i == IAgoraAPI.ECODE_LOGOUT_E_KICKED) { //other login the account
+//                                WritableMap map = Arguments.createMap();
+//                                map.putString("type", "onLogoutForceout");
+//                                map.putInt("code", i);
+//                                commonEvent(map);
+//
+//                            } else if (i == IAgoraAPI.ECODE_LOGOUT_E_NET) { //net
+//                                WritableMap map = Arguments.createMap();
+//                                map.putString("type", "onLogoutForNetwork");
+//                                map.putInt("code", i);
+//                                commonEvent(map);
+//                            }else {
                                 WritableMap map = Arguments.createMap();
                                 map.putString("type", "onLogout");
                                 map.putInt("code", i);
                                 commonEvent(map);
-                            }
+                           // }
                         }
                     });
 
@@ -268,6 +320,22 @@ public class SignalingModule extends ReactContextBaseJavaModule {
                             map.putString("type", "onQueryUserStatusResult");
                             map.putString("status", status);
                             map.putString("name", name);
+                            commonEvent(map);
+                        }
+                    });
+                }
+
+                // query channel user nums
+                @Override
+                public void onChannelQueryUserNumResult(final String channelID, final int ecode, final int num) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onChannelQueryUserNumResult");
+                            map.putString("channelID", channelID);
+                            map.putInt("ecode", ecode);
+                            map.putInt("num", num);
                             commonEvent(map);
                         }
                     });
@@ -337,12 +405,162 @@ public class SignalingModule extends ReactContextBaseJavaModule {
                     });
                 }
 
+                // get call invited
+                @Override
+                public void onInviteReceived(final String channelID, final String account,final int uid, final String extra) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onInviteReceived");
+                            map.putString("channelID", channelID);
+                            map.putString("account", account);
+                            map.putInt("uid", uid);
+                            map.putString("extra", extra);
+                            commonEvent(map);
+                        }
+                    });
+                }
 
+
+                // your call arrived
+                @Override
+                public void onInviteReceivedByPeer(final String channelID, final String account,final int uid) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onInviteReceivedByPeer");
+                            map.putString("channelID", channelID);
+                            map.putString("account", account);
+                            map.putInt("uid", uid);
+                            commonEvent(map);
+                        }
+                    });
+                }
+
+                // your call received by peer
+                @Override
+                public void onInviteAcceptedByPeer(final String channelID, final String account,final int uid,  final String extra) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onInviteAcceptedByPeer");
+                            map.putString("channelID", channelID);
+                            map.putString("account", account);
+                            map.putInt("uid", uid);
+                            map.putString("extra", extra);
+                            commonEvent(map);
+                        }
+                    });
+                }
+
+                // your call refused by peer
+                @Override
+                public void onInviteRefusedByPeer(final String channelID, final String account,final int uid,  final String extra) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onInviteRefusedByPeer");
+                            map.putString("channelID", channelID);
+                            map.putString("account", account);
+                            map.putInt("uid", uid);
+                            map.putString("extra", extra);
+                            commonEvent(map);
+                        }
+                    });
+                }
+
+                // invite error
+                @Override
+                public void onInviteFailed(final String channelID, final String account,final int uid, final int ecode, final String extra) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onInviteFailed");
+                            map.putString("channelID", channelID);
+                            map.putString("account", account);
+                            map.putInt("uid", uid);
+                            map.putInt("ecode", ecode);
+                            map.putString("extra", extra);
+                            commonEvent(map);
+                        }
+                    });
+                }
+
+
+                // onInviteEndByPeer
+                @Override
+                public void onInviteEndByPeer(final String channelID, final String account,final int uid, final String extra) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onInviteEndByPeer");
+                            map.putString("channelID", channelID);
+                            map.putString("account", account);
+                            map.putInt("uid", uid);
+                            map.putString("extra", extra);
+                            commonEvent(map);
+                        }
+                    });
+                }
+
+                // onInviteEndByMyself
+                @Override
+                public void onInviteEndByMyself(final String channelID, final String account,final int uid) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onInviteEndByMyself");
+                            map.putString("channelID", channelID);
+                            map.putString("account", account);
+                            map.putInt("uid", uid);
+                            commonEvent(map);
+                        }
+                    });
+                }
+
+
+                // onInviteMsg
+                @Override
+                public void onInviteMsg(final String channelID, final String account,final int uid, final String msgType, final String msgData, final String extra) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onInviteMsg");
+                            map.putString("channelID", channelID);
+                            map.putString("account", account);
+                            map.putInt("uid", uid);
+                            map.putString("msgType", msgType);
+                            map.putString("msgData", msgData);
+                            map.putString("extra", extra);
+                            commonEvent(map);
+                        }
+                    });
+                }
 
 
                 @Override
                 public void onError(String s, int i, String s1) {
                     Log.i(TAG, "onError s:" + s + " s1:" + s1);
+                    // 把所有的错误日志收集起来
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("type", "onError");
+                            map.putString("name", s);
+                            map.putInt("ecode", i);
+                            map.putString("desc", s1);
+                            commonEvent(map);
+                        }
+                    });
                 }
 
             });
